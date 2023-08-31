3beebeeadae84bd99e19f7badd72fc619c3c7ba6
bob-g12 <sasakiT1019s@outlook.jp>
2023-09-01 00:36:40 +0900

    サーバーの仕組み(イメージ)

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 print("Hello,world")
 input_1 = input()
 print(input_1)
+while True:
+    input_2 = input()
+    if input_2 == "http://localhost:3000/":
+        if "ネットワークエラー":
+            print("GET / HTTP/1.1 503 xxx`")
+        if "webページが見つからない":
+            print("GET / HTTP/1.1 404 xxx`")
+        print("GET / HTTP/1.1 200 xxx`")    
+    if input_2 == "http://localhost:3000/favicon.ico":
+        print("GET /favicon.ico HTTP/1.1 404 xxx`")
