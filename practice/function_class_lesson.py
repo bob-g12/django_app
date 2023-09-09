c00fa94ffff29090e5b8ff76a36ab5b4e5e0680f
bob-g12 <sasakiT1019s@outlook.jp>
2023-09-09 17:15:16 +0900

    変数名修正

--- practice/function_class_lesson.py
+++ practice/function_class_lesson.py
@@ -5,6 +5,7 @@ print("はろわ")
 print(60 / 1.7 **2)
 
 #　BMIを算出するプログラムの入力を可変可能にする
-debu = int(input("体重:"))
-tibi = float(input("身長:"))
-print(debu / tibi **2)
+kg = int(input("体重:"))
+m = float(input("身長:"))
+print(kg / m **2)
+
