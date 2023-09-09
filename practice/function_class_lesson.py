dd9acfde5fe5a9fdad960d26ee15c0afac20f355
bob-g12 <sasakiT1019s@outlook.jp>
2023-09-09 17:10:27 +0900

    BMIプログラムを可変式に変更

--- practice/function_class_lesson.py
+++ practice/function_class_lesson.py
@@ -3,3 +3,8 @@ print("はろわ")
 # 体重（kg）÷｛身長（m）の2乗｝
 # 体重＝60　身長＝　170の場合のBMIを算出するプログラム
 print(60 / 1.7 **2)
+
+#　BMIを算出するプログラムの入力を可変可能にする
+debu = int(input("体重:"))
+tibi = float(input("身長:"))
+print(debu / tibi **2)
