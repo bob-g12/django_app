2cac7ba00bc7f5e98e31e6b5dca408eb0283f167
bob-g12 <sasakiT1019s@outlook.jp>
2023-09-10 17:32:42 +0900

    フォルダ整理

---
+++ practice/lesson_function_class/human_list.py
@@ -0,0 +1,11 @@
+san = "くん"
+gobi = "。"
+status_name = ["BMI","身長","体重","足のサイズ"]
+for i in range(4):
+    status = input("名前,身長,体重,足:").split()
+    m = int(status[1]) / 100
+    kg = int(status[2])
+    print(status[0]+san+"の"+status_name[1]+"は"+str(status[1])+"(cm)"+gobi)
+    print(status[0]+san+"の"+status_name[2]+"は"+str(kg)+"(kg)"+gobi)
+    print(status[0]+san+"の"+status_name[0]+"は"+str(kg / m **2)+"(kg/m2)"+gobi)
+    print(status[0]+san+"の"+status_name[3]+"は"+status[3]+"(kg)"+gobi)
