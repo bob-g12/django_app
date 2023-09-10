9cef48cb8071c6aa8618ecf1691d0e384ed02615
bob-g12 <sasakiT1019s@outlook.jp>
2023-09-10 20:33:40 +0900

    入力の可変の対応

--- practice/lesson_function_class/human_list.py
+++ practice/lesson_function_class/human_list.py
@@ -1,23 +1,33 @@
 san = "さん"
 gobi = ""
-count = 0
-status_name = ["BMI","身長","体重","足のサイズ","職業","職歴","血液型"]
+status_name = ["BMI","身長","体重","足のサイズ","職業","職歴","公開"]
 file = open('input.txt','r', encoding="utf-8")
+status = []
+status_len = 0
+counter = 0
+i = 0
 
-for i in range(4):
-    count += 1
+while True:
+    status.append(file.readline().split())
+
+    if status[i] == []:
+        break
+    status_len = len(status)
+    i += 1
+count = i
+
 print(str(count)+"人の名簿を受け取りました。")
 
-for i in range(4):
-    status = file.readline().split()
-    m = int(status[1]) / 100
-    kg = int(status[2])
+for i in range(count):
+    m = int(status[i][1]) / 100
+    kg = int(status[i][2])
 
-    print(status[0]+san+"の"+status_name[1]+"は"+str(status[1])+"(cm)"+gobi)
-    print(status[0]+san+"の"+status_name[2]+"は"+str(kg)+"(kg)"+gobi)
-    print(status[0]+san+"の"+status_name[0]+"は"+str(kg / m **2)+"(kg/m2)"+gobi)
-    print(status[0]+san+"の"+status_name[3]+"は"+status[3]+"(kg)"+gobi)
-    print(status[0]+san+"の"+status_name[4]+"は"+status[4]+gobi)
-    print(status[0]+san+"の"+status_name[5]+"は"+status[5]+"年"+gobi)
-    print(status[0]+san+"の"+status_name[6]+"は"+status[6]+"型"+gobi)
+    print(status[i][0]+san+"の"+status_name[1]+"は"+str(status[i][1])+"(cm)"+gobi)
+    print(status[i][0]+san+"の"+status_name[2]+"は"+str(kg)+"(kg)"+gobi)
+    print(status[i][0]+san+"の"+status_name[0]+"は"+str(kg / m **2)+"(kg/m2)"+gobi)
+    print(status[i][0]+san+"の"+status_name[3]+"は"+status[i][3]+"(kg)"+gobi)
+    print(status[i][0]+san+"の"+status_name[4]+"は"+status[i][4]+gobi)
+    print(status[i][0]+san+"の"+status_name[5]+"は"+status[i][5]+"年"+gobi)
+
 file.close()
+
