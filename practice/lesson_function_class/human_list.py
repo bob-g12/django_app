a7f307ac18b236ccce681dbd3fc295fbae975e3d
bob-g12 <sasakiT1019s@outlook.jp>
2023-09-10 20:51:23 +0900

    公開条件の設定

--- practice/lesson_function_class/human_list.py
+++ practice/lesson_function_class/human_list.py
@@ -6,28 +6,33 @@ status = []
 status_len = 0
 counter = 0
 i = 0
+open_list = 0
 
 while True:
     status.append(file.readline().split())
 
     if status[i] == []:
         break
+    if status[i][6] == "公開":
+        open_list += 1
     status_len = len(status)
     i += 1
+    
 count = i
 
-print(str(count)+"人の名簿を受け取りました。")
+print(str(open_list)+"人の名簿を受け取りました。")
 
 for i in range(count):
     m = int(status[i][1]) / 100
     kg = int(status[i][2])
 
-    print(status[i][0]+san+"の"+status_name[1]+"は"+str(status[i][1])+"(cm)"+gobi)
-    print(status[i][0]+san+"の"+status_name[2]+"は"+str(kg)+"(kg)"+gobi)
-    print(status[i][0]+san+"の"+status_name[0]+"は"+str(kg / m **2)+"(kg/m2)"+gobi)
-    print(status[i][0]+san+"の"+status_name[3]+"は"+status[i][3]+"(kg)"+gobi)
-    print(status[i][0]+san+"の"+status_name[4]+"は"+status[i][4]+gobi)
-    print(status[i][0]+san+"の"+status_name[5]+"は"+status[i][5]+"年"+gobi)
+    if status[i][6] == "公開":
+        print(status[i][0]+san+"の"+status_name[1]+"は"+str(status[i][1])+"(cm)"+gobi)
+        print(status[i][0]+san+"の"+status_name[2]+"は"+str(kg)+"(kg)"+gobi)
+        print(status[i][0]+san+"の"+status_name[0]+"は"+str(kg / m **2)+"(kg/m2)"+gobi)
+        print(status[i][0]+san+"の"+status_name[3]+"は"+status[i][3]+"(kg)"+gobi)
+        print(status[i][0]+san+"の"+status_name[4]+"は"+status[i][4]+gobi)
+        print(status[i][0]+san+"の"+status_name[5]+"は"+status[i][5]+"年"+gobi)
 
 file.close()
 
