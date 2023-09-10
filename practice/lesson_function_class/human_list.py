970a9656d159b73984ad54bc2f56997c0f776e1a
bob-g12 <sasakiT1019s@outlook.jp>
2023-09-10 22:11:14 +0900

    オプションで表示する項目を指定

--- practice/lesson_function_class/human_list.py
+++ practice/lesson_function_class/human_list.py
@@ -1,6 +1,11 @@
 import sys
 
 if  len(sys.argv) <= 1:
+    print("")
+elif sys.argv[1] == "-h":
+    print("Hello")
+
+elif  sys.argv[1] == "-m":
     
     san = "さん"
     gobi = ""
@@ -11,6 +16,7 @@ if  len(sys.argv) <= 1:
     counter = 0
     i = 0
     open_list = 0
+    check_list =[]
 
     while True:
         status.append(file.readline().split())
@@ -24,20 +30,32 @@ if  len(sys.argv) <= 1:
         
     count = i
 
-    print(str(open_list)+"人の名簿を受け取りました。")
+
+    for i in range(len(status_name)):
+        judge = "NO"
+        for j in range(len(sys.argv)):
+            if status_name[i] == sys.argv[j]:
+                judge = "YES"
+        check_list.append(judge)
+    if sys.argv[1] != "-m":
+        print(str(open_list)+"人の名簿を受け取りました。")
 
     for i in range(count):
         m = int(status[i][1]) / 100
         kg = int(status[i][2])
 
         if status[i][6] == "公開":
-            print(status[i][0]+san+"の"+status_name[1]+"は"+str(status[i][1])+"(cm)"+gobi)
-            print(status[i][0]+san+"の"+status_name[2]+"は"+str(kg)+"(kg)"+gobi)
-            print(status[i][0]+san+"の"+status_name[0]+"は"+str(kg / m **2)+"(kg/m2)"+gobi)
-            print(status[i][0]+san+"の"+status_name[3]+"は"+status[i][3]+"(kg)"+gobi)
-            print(status[i][0]+san+"の"+status_name[4]+"は"+status[i][4]+gobi)
-            print(status[i][0]+san+"の"+status_name[5]+"は"+status[i][5]+"年"+gobi)
+            if check_list[1] == "YES":
+                print(status[i][0]+san+"の"+status_name[1]+"は"+str(status[i][1])+"(cm)"+gobi)
+            if check_list[2] == "YES":
+                print(status[i][0]+san+"の"+status_name[2]+"は"+str(kg)+"(kg)"+gobi)
+            if check_list[0] == "YES":
+                print(status[i][0]+san+"の"+status_name[0]+"は"+str(kg / m **2)+"(kg/m2)"+gobi)
+            if check_list[3] == "YES":
+                print(status[i][0]+san+"の"+status_name[3]+"は"+status[i][3]+"(kg)"+gobi)
+            if check_list[4] == "YES":
+                print(status[i][0]+san+"の"+status_name[4]+"は"+status[i][4]+gobi)
+            if check_list[5] == "YES":
+                print(status[i][0]+san+"の"+status_name[5]+"は"+status[i][5]+"年"+gobi)
 
     file.close()
-elif sys.argv[1] == "-h":
-    print("Hello")
