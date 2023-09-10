cfc57e41239c2e8849b148c461305057dcb3f833
bob-g12 <sasakiT1019s@outlook.jp>
2023-09-10 18:07:15 +0900

    BMIプログラムの表示に職業,職歴,血液型を追加

--- practice/lesson_function_class/human_list.py
+++ practice/lesson_function_class/human_list.py
@@ -1,14 +1,14 @@
 san = "さん"
 gobi = ""
 count = 0
-status_name = ["BMI","身長","体重","足のサイズ"]
+status_name = ["BMI","身長","体重","足のサイズ","職業","職歴","血液型"]
 
 for i in range(4):
     count += 1
 print(str(count)+"人の名簿を受け取りました。")
 
 for i in range(4):
-    status = input("名前,身長,体重,足:").split()
+    status = input("名前,身長,体重,足,職業,職歴,血液型:").split()
     m = int(status[1]) / 100
     kg = int(status[2])
 
@@ -16,3 +16,6 @@ for i in range(4):
     print(status[0]+san+"の"+status_name[2]+"は"+str(kg)+"(kg)"+gobi)
     print(status[0]+san+"の"+status_name[0]+"は"+str(kg / m **2)+"(kg/m2)"+gobi)
     print(status[0]+san+"の"+status_name[3]+"は"+status[3]+"(kg)"+gobi)
+    print(status[0]+san+"の"+status_name[4]+"は"+status[4]+gobi)
+    print(status[0]+san+"の"+status_name[5]+"は"+status[5]+"年"+gobi)
+    print(status[0]+san+"の"+status_name[6]+"は"+status[6]+"型"+gobi)
