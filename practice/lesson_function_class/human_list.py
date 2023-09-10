e93cf5e204f884c179cb433e2429ed8796fb01f2
bob-g12 <sasakiT1019s@outlook.jp>
2023-09-10 21:19:22 +0900

    オプションの追加

--- practice/lesson_function_class/human_list.py
+++ practice/lesson_function_class/human_list.py
@@ -1,38 +1,43 @@
-san = "さん"
-gobi = ""
-status_name = ["BMI","身長","体重","足のサイズ","職業","職歴","公開"]
-file = open('input.txt','r', encoding="utf-8")
-status = []
-status_len = 0
-counter = 0
-i = 0
-open_list = 0
+import sys
 
-while True:
-    status.append(file.readline().split())
-
-    if status[i] == []:
-        break
-    if status[i][6] == "公開":
-        open_list += 1
-    status_len = len(status)
-    i += 1
+if  len(sys.argv) <= 1:
     
-count = i
+    san = "さん"
+    gobi = ""
+    status_name = ["BMI","身長","体重","足のサイズ","職業","職歴","公開"]
+    file = open('input.txt','r', encoding="utf-8")
+    status = []
+    status_len = 0
+    counter = 0
+    i = 0
+    open_list = 0
 
-print(str(open_list)+"人の名簿を受け取りました。")
+    while True:
+        status.append(file.readline().split())
 
-for i in range(count):
-    m = int(status[i][1]) / 100
-    kg = int(status[i][2])
+        if status[i] == []:
+            break
+        if status[i][6] == "公開":
+            open_list += 1
+        status_len = len(status)
+        i += 1
+        
+    count = i
 
-    if status[i][6] == "公開":
-        print(status[i][0]+san+"の"+status_name[1]+"は"+str(status[i][1])+"(cm)"+gobi)
-        print(status[i][0]+san+"の"+status_name[2]+"は"+str(kg)+"(kg)"+gobi)
-        print(status[i][0]+san+"の"+status_name[0]+"は"+str(kg / m **2)+"(kg/m2)"+gobi)
-        print(status[i][0]+san+"の"+status_name[3]+"は"+status[i][3]+"(kg)"+gobi)
-        print(status[i][0]+san+"の"+status_name[4]+"は"+status[i][4]+gobi)
-        print(status[i][0]+san+"の"+status_name[5]+"は"+status[i][5]+"年"+gobi)
+    print(str(open_list)+"人の名簿を受け取りました。")
 
-file.close()
+    for i in range(count):
+        m = int(status[i][1]) / 100
+        kg = int(status[i][2])
 
+        if status[i][6] == "公開":
+            print(status[i][0]+san+"の"+status_name[1]+"は"+str(status[i][1])+"(cm)"+gobi)
+            print(status[i][0]+san+"の"+status_name[2]+"は"+str(kg)+"(kg)"+gobi)
+            print(status[i][0]+san+"の"+status_name[0]+"は"+str(kg / m **2)+"(kg/m2)"+gobi)
+            print(status[i][0]+san+"の"+status_name[3]+"は"+status[i][3]+"(kg)"+gobi)
+            print(status[i][0]+san+"の"+status_name[4]+"は"+status[i][4]+gobi)
+            print(status[i][0]+san+"の"+status_name[5]+"は"+status[i][5]+"年"+gobi)
+
+    file.close()
+elif sys.argv[1] == "-h":
+    print("Hello")
