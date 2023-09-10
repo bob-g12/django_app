c72d27c80ebecb49e26380c2f966e2d84fb63e9c
bob-g12 <sasakiT1019s@outlook.jp>
2023-09-10 17:55:54 +0900

    BMIプログラムの入力数表示

--- practice/lesson_function_class/human_list.py
+++ practice/lesson_function_class/human_list.py
@@ -1,10 +1,17 @@
-san = "くん"
-gobi = "。"
+san = "さん"
+gobi = ""
+count = 0
 status_name = ["BMI","身長","体重","足のサイズ"]
+
+for i in range(4):
+    count += 1
+print(str(count)+"人の名簿を受け取りました。")
+
 for i in range(4):
     status = input("名前,身長,体重,足:").split()
     m = int(status[1]) / 100
     kg = int(status[2])
+
     print(status[0]+san+"の"+status_name[1]+"は"+str(status[1])+"(cm)"+gobi)
     print(status[0]+san+"の"+status_name[2]+"は"+str(kg)+"(kg)"+gobi)
     print(status[0]+san+"の"+status_name[0]+"は"+str(kg / m **2)+"(kg/m2)"+gobi)
